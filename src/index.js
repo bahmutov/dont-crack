612af5a306f80321468d722b336362caed810859
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-12-21 00:20:03 -0500

    chore(code): moved source file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
-'use strict'
+const log = require('debug')('dont-crack')
+const is = require('check-more-types')
+const la = require('lazy-ass')
+const dontBreak = require('dont-break')
+const alwaysError = require('always-error')
+
+const isReleaseType = is.oneOf(['major', 'minor', 'patch'])
+
+function dontCrack (opts, config, callback) {
+  log('dont-crack arguments')
+  log('opts', opts)
+  log('commits')
+  log(config.commits)
+  log('last release was', config.lastRelease)
+  log('next release is', config.nextRelease)
+
+  la(isReleaseType(config.nextRelease.type),
+    'invalid next release type', config.nextRelease)
+
+  if (config.nextRelease.type === 'major') {
+    log('major release will crack dependents by definition')
+    log('no need to check.')
+    return callback()
+  }
+
+  log('%s release should not break dependents', config.nextRelease.type)
+
+  function finishedChecking (success) {
+    console.log('dont-crack finishing with success?', success)
+    if (success) {
+      return callback(null)
+    }
+    const type = config.nextRelease.type
+    const from = config.lastRelease.version
+    const to = config.nextRelease.version
+    const s = `Upgrade "${type}" from ${from} to ${to} is cracking dependents.`
+    callback(new Error(s))
+  }
+
+  dontBreak()
+    .catch(function (err) {
+      console.error('dont-break error?', err)
+      callback(alwaysError(err))
+    })
+    .then(finishedChecking)
+    .done()
+}
+module.exports = dontCrack
